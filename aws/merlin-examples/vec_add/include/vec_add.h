8ad90e52b33ccd9eb9625f7c660925479b553b65
JunYao <junyao@falcon-computing.com>
2018-05-11 04:39:14 -0700

    add merlin example

---
+++ aws/merlin-examples/vec_add/include/vec_add.h
@@ -0,0 +1,10 @@
+/****************************************************************************/
+// vec_add.hpp
+// Copyright (C) 2015-2018 Falcon Computing Solutions, Inc. - All rights reserved.
+/****************************************************************************/
+
+#define VEC_SIZE 10000
+
+void vec_add(int *a, int *b, int*c, int inc);
+void vec_add_kernel(int *a, int *b, int*c, int inc);
+
