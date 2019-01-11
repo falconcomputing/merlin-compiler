0d2b0dec018444e4f140f2625a8f5dd430d4670e
mo-eissa <moheissa@falcon-computing.com>
2019-01-11 12:21:54 -0800

    Merging the common makefile to work with the examples.

--- Examples/common/mcc_common.mk
+++ Examples/common/mcc_common.mk
@@ -9,7 +9,9 @@ MAKEFLAGS += --no-print-directory
 #####################################################################
 
 # Modify target platform to your choosing
-ifeq ($(VENDOR),XILINX)
+ifeq ($(VENDOR),CPU)
+     $(info This is CPU flow only - No FPGA compile)
+else ifeq ($(VENDOR),XILINX)
     ifeq ($(DEVICE),F1)
         PLATFORM=sdaccel::xilinx:aws-vu9p-f1:4ddr-xpr-2pr:4.0
     else ifeq ($(DEVICE),)
@@ -62,9 +64,9 @@ SIMGEN_OUTPUT=$(KERNEL_NAME)_sim
 ACCHW_BUILD_TIME=$(shell date +%Y%m%d_%H%M%S)
 BITGEN_OUTPUT=$(KERNEL_NAME)_hw.$(BIN_EXT)
 BITGEN_OUTPUT_TIMESTAMPED=$(KERNEL_NAME)_$(ACCHW_BUILD_TIME).$(BIN_EXT)
-AFIGEN_OUTPUT=$(USER)_$(KERNEL_NAME)_hw
-AFIGEN_OUTPUT_TIMESTAMPED=$(USER)_$(KERNEL_NAME)_hw_$(ACCHW_BUILD_TIME).awsxclbin
-ACC_PKG=$(KERNEL_NAME)_pkg
+AFIGEN_OUTPUT=$(KERNEL_NAME)_hw
+AFIGEN_OUTPUT_TIMESTAMPED=$(KERNEL_NAME)_hw_$(ACCHW_BUILD_TIME).awsxclbin
+ACC_PKG=$(EXE)_pkg
 ACC_PKG_FILE=$(ACC_PKG).tar.gz
 
 #####################################################################
@@ -74,20 +76,27 @@ ACC_PKG_FILE=$(ACC_PKG).tar.gz
 .PHONY: default clean
 
 default:
-	@echo "**** Merlin Compiler Makefile"
-	@echo "**** Copyright (C) 2015-2018 Falcon Computing Solutions, Inc. - All rights reserved."
-	@echo ""
-	@echo "$$ make <target>"
-	@echo "Available targets:"
-	@echo "           run - Compile and Run TB Executable on CPU (without Acceleration)"
-	@echo "       mcc_acc - Use Merlin Compiler to compile the Acceleration Kernel code"
-	@echo "    mcc_runsim - Generate kernel binary for simulation on CPU and run it"
-	@echo "  mcc_estimate - Get resource and performance estimates"
-	@echo "    mcc_bitgen - Generate kernel binary for Acceleration Platform"
-	@echo "     mcc_runhw - Run TB Executable on Host CPU and accelerated kernel on platform HW"
-	@echo "         clean - Remove all output products (except for bitgen outputs)"
-
-all: run runsim estimate bitgen 
+	@echo "**************************************************************************************"
+	@echo "* Merlin Compiler Makefile                                                           *"
+	@echo "* Copyright (C) 2015-2019 Falcon Computing Solutions, Inc. - All rights reserved.    *"
+	@echo "*                                                                                    *"
+	@echo "* Usage: make <target>                                                               *"
+	@echo "* Available targets:                                                                 *"
+ifeq ($(VENDOR),CPU)
+	@echo "*          run - Compile and run executable on CPU (without Acceleration)            *"
+else
+	@echo "*   mcc_runsim - Generate kernel binary for simulation on CPU and run it             *"      
+	@echo "* mcc_estimate - Get resource and performance estimates                              *"
+	@echo "*   mcc_bitgen - Generate kernel binary for Acceleration Platform                    *"
+	@echo "*   mcc_accexe - Run rxecutable on Host CPU and accelerated kernel on platform       *"
+	@echo "*    mcc_runhw - Run executable on host CPU and accelerated kernel on platform FPGA  *"
+ifeq ($(VENDOR),XILINX)
+	@echo "*   mcc_afigen - Generate AFI for AWS F1 FPGA (AWS F1 users)                         *"
+	@echo "*      mcc_pkg - Package all necessary files for running accelerated application     *"
+endif
+endif
+	@echo "*        clean - Remove all output products (except for bitgen outputs)              *"
+	@echo "**************************************************************************************"
 
 mcc_runhw: $(BITGEN_OUTPUT) $(ACC_EXE) 
 	./$(ACC_EXE) $(EXE_ARGS) $(BITGEN_OUTPUT) 
@@ -124,18 +133,21 @@ mcc_accsimexe $(ACC_SIM_EXE): $(HOST_SRC_FILES) $(KERNEL_SRC_FILES)
 
 mcc_acc : $(ACCGEN_OUTPUT).mco
 
-mcc_pkg : $(ACC_PKG_FILE)
+mcc_pkg : $(ACC_PKG_FILE) 
 
-$(ACC_PKG_FILE) : $(ACC_EXE)  $(BITGEN_OUTPUT)
-	mkdir -p $(ACC_PKG)
+$(ACC_PKG_FILE) : $(ACC_EXE)
+	mkdir -p ./$(ACC_PKG)
+	cp -r $(DATA_DIR) $(ACC_PKG)
 	cp $(ACC_EXE) $(ACC_PKG)
 	cp $(BITGEN_OUTPUT) $(ACC_PKG)
-	cp $(KERNEL_NAME)_hw.gbs $(ACC_PKG) 2>/dev/null || :
-	cp $(AFIGEN_OUTPUT).awsxclbin $(ACC_PKG) 2>/dev/null || :
+	cp $(AFIGEN_OUTPUT).awsxclbin $(ACC_PKG) 2>/dev/null
 	cp lib$(KERNEL_NAME).so $(ACC_PKG)
-	tar cf $(ACC_PKG).tar $(ACC_PKG)
-	gzip $(ACC_PKG).tar
+	echo "mcc_runhw: " >  ./$(ACC_PKG)/Makefile
+	echo -e "\t./$(ACC_EXE) $(F1_EXE_ARGS) $(AFIGEN_OUTPUT).awsxclbin" >>  ./$(ACC_PKG)/Makefile
+	tar czf $(ACC_PKG_FILE) $(ACC_PKG)
 	echo "Package $(ACC_PKG_FILE) is ready"
+	rm -rf $(ACC_PKG)
+
 
 $(ACCGEN_OUTPUT).mco : $(KERNEL_SRC_FILES)
 	merlincc -c $^ -D $(VENDOR) -o $(ACCGEN_OUTPUT) $(CMP_OPT) $(KERNEL_INC_DIR) --platform=$(PLATFORM)
@@ -156,13 +168,14 @@ $(KERNEL_SRC_FILES) :
 	echo ""
 
 $(AFIGEN_OUTPUT).awsxclbin : $(BITGEN_OUTPUT) 
-	$(SDK_DIR)/SDAccel/tools/create_sdaccel_afi.sh -xclbin=$(BITGEN_OUTPUT) \
+	$(MCC_COMMON_DIR)/create_sdaccel_afi.sh -xclbin=$(BITGEN_OUTPUT) \
 	             -o=$(AFIGEN_OUTPUT) -s3_bucket=fcs-fpga -s3_dcp_key=dcp -s3_logs_key=log
 	cp $(AFIGEN_OUTPUT).awsxclbin $(AFIGEN_OUTPUT_TIMESTAMPED) 
 
 clean:
 	rm -rfv merlin.log *.o $(SRC_DIR)/data_int*.txt $(EXE)
 	rm -rf $(ACCGEN_OUTPUT).mco $(SIMGEN_OUTPUT).mco $(ACC_EXE) $(ACC_SIM_EXE) $(SIMGEN_OUTPUT).$(BIN_EXT) emconfig.json $(EST_OUTPUT) lib$(KERNEL_NAME)_sim.so lib$(KERNEL_NAME).so __merlin$(KERNEL_NAME).h
+
 
 
 ####################################################################
@@ -207,4 +220,3 @@ xoc_bitgen : $(XOC_BITGEN_OUTPUT)
 
 $(XOC_BITGEN_OUTPUT) : $(XOC_KERNEL_SRC_FILES)
 	xocc -t hw --platform $(XDEVICE) $^ $(XOC_OPT) $(KERNEL_INC_DIR) -o $@
-
